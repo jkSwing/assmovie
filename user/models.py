aabe8e1eb7d7b28e810a0e4192a9d1f244c94071
yangjinpeng.jp <yangjinpeng.jp@bytedance.com>
2019-09-20 17:38:09 +0800

    shit I forgot migrations

--- user/models.py
+++ user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)
-    salt = models.CharField(max_length=100)
+    name = models.CharField(max_length=50, unique=True, null=False)
+    password = models.CharField(max_length=100, null=False)
+    salt = models.CharField(max_length=100, null=False)
 
     class Meta:
         db_table = 'user'
